ceb18d2d44e6e590da3d914fe0ccd180c600d726
Tim Wilde <git@timwil.de>
2022-09-04 21:47:02 +0100

    Top level router container not required with wouter.

--- FrontEnd.SPA/ClientApp/src/index.js
+++ FrontEnd.SPA/ClientApp/src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Router} from "wouter";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-    <Router>
-        <App/>
-    </Router>
-);
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
