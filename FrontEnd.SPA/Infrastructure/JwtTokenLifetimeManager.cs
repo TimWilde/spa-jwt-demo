7505ac1a24bc3401015d2ca06779d9774d7e76bf
Tim Wilde <git@timwil.de>
2022-09-11 23:33:45 +0100

    Added rudimentary signature expiry for logged out tokens.

--- FrontEnd.SPA/Infrastructure/JwtTokenLifetimeManager.cs
+++ FrontEnd.SPA/Infrastructure/JwtTokenLifetimeManager.cs
@@ -1,23 +1,28 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System.Collections.Concurrent;
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 
 namespace FrontEnd.SPA.Infrastructure;
 
 public class JwtTokenLifetimeManager : ITokenLifetimeManager
 {
-   private static readonly HashSet<string> DisavowedSignatures = new();
+   private static readonly ConcurrentDictionary<string, DateTime> DisavowedSignatures = new();
 
    public bool ValidateTokenLifetime( DateTime? notBefore,
                                       DateTime? expires,
                                       SecurityToken securityToken,
                                       TokenValidationParameters validationParameters ) =>
       securityToken is JwtSecurityToken token &&
+      token.ValidFrom <= DateTime.UtcNow &&
       token.ValidTo >= DateTime.UtcNow &&
-      !DisavowedSignatures.Contains( token.RawSignature );
+      !DisavowedSignatures.ContainsKey( token.RawSignature );
 
    public void SignOut( SecurityToken securityToken )
    {
       if ( securityToken is JwtSecurityToken token )
-         DisavowedSignatures.Add( token.RawSignature );
+         DisavowedSignatures.TryAdd( token.RawSignature, token.ValidTo );
+
+      foreach ( ( string? key, DateTime _ ) in DisavowedSignatures.Where( x => x.Value <= DateTime.UtcNow ) )
+         DisavowedSignatures.TryRemove( key, out DateTime _ );
    }
 }
